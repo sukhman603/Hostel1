ffadccb1b2b5efec634f7b1b6b74fc2a2c5a5a87
sukhman603 <74083904+sukhman603@users.noreply.github.com>
2020-12-05 23:05:52 +0530

    Add files via upload

---
+++ Hostel_main-20201205T170623Z-001/Hostel_main/src/hostel_main/Hostel_main.java
@@ -0,0 +1,12 @@
+
+package hostel_main;
+
+
+public class Hostel_main {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Hostel_main-20201205T170623Z-001/Hostel_main/src/hostel_main/connection_details.java
@@ -0,0 +1,10 @@
+
+package hostel_main;
+
+
+public interface connection_details {
+    String path="jdbc:mysql//local host/";
+    String place="hostel";
+    String username="root";
+    String password="";
+}
